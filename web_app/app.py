0905574076f19b78d8db927d328ce006951f98d9
msm1723 <msm1723@gmail.com>
2023-05-20 17:58:44 +0300

    nothing

--- web_app/app.py
+++ web_app/app.py
@@ -1,10 +1,10 @@
 import subprocess 
 from src import create_app
 
-app = create_app()
-
 # wait for db to be ready
 subprocess.run(['sh', './web_app/wait_for_db.sh'])
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
